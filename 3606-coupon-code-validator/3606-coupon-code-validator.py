e7cc467c36c49afc03d9deb73b0dc0c65bcc86c9
Disha Kunjadia <66826771+dishakunjadia@users.noreply.github.com>
2025-12-12 19:29:33 -0500

    Time: 7 ms (71.38%), Space: 18 MB (51.45%) - LeetHub

---
+++ 3606-coupon-code-validator/3606-coupon-code-validator.py
@@ -0,0 +1,33 @@
+from typing import List
+class Solution:
+    def validateCoupons(self, code: List[str], businessLine: List[str], isActive: List[bool]) -> List[str]:
+        order = {
+            "electronics": 0,
+            "grocery": 1,
+            "pharmacy": 2,
+            "restaurant": 3
+        }
+
+        valid = []
+
+        for c, b, active in zip(code, businessLine, isActive):
+            if not active:
+                continue
+
+            if b not in order:
+                continue
+
+            if not c:
+                continue
+
+            # check alphanumeric or underscore
+            if not all(ch.isalnum() or ch == '_' for ch in c):
+                continue
+
+            valid.append((order[b], c))
+
+        # sort by businessLine order, then lexicographically by code
+        valid.sort()
+
+        return [c for _, c in valid]
+        
