17a8ae01b93b729a2de79df708cacd2bae8926e1
Disha Kunjadia <66826771+dishakunjadia@users.noreply.github.com>
2026-09-08 04:27:31 -0400

    Time: 0 ms (100%), Space: 10.7 MB (57.08%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> result(n+1);
-        
-        if (n == 0 )
-            return result;
-        result[0] = 0;
-        for ( int i = 1;i <= n; i++){
-            if (i%2 !=0){
-                result[i] = result[i/2] + 1;
-            } else {
-                result[i] = result[i/2];
-            }
+
+        if (n == 0)
+        return result;
+
+        for (int i =0; i < n + 1; i++) {
+            result[i] = result[i>>1] + (i & 1);
         }
         return result;
     }
+    
 };
